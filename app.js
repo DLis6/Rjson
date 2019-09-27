c9e58b1788bd13abb7b350a7d54d1c3d0c0df691
David <54381677+DLis6@users.noreply.github.com>
2019-09-26 20:03:27 -0500

    ReturnJson

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/products', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 
 var schema = mongoose.Schema({
@@ -15,6 +15,7 @@ var Products = mongoose.model("Productos", schema);
 app.get('/products', async(req, res) => { //Muestra constendo json de los libros.
 
     var info = await Products.find({}).exec();
+
     res.json(info);
 });
 
